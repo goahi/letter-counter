0dca1963d396e78d6b35a26485d1357530682787
goahi <goai@protonmail.com>
2022-03-13 14:51:52 +0900

    chore: fix svelte.config for cloudlfare again

--- svelte.config.js
+++ svelte.config.js
@@ -8,9 +8,7 @@ const config = {
     preprocess: preprocess(),
 
     kit: {
-        adapter: adapter({
-            precompress: false,
-        }),
+        adapter: adapter(),
 
         prerender: {
             default: true,
